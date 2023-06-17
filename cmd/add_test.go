9adf8b142ecd3016569cb4db02b48be090f32087
Luis Güette <guetteluis@gmail.com>
2023-06-17 16:24:00 -0400

    add new test

--- cmd/add_test.go
+++ cmd/add_test.go
@@ -37,4 +37,32 @@ func TestAdd(t *testing.T) {
 		app.Config.(*core.ConfigMock).AssertExpectations(t)
 		app.Exec.(*utils.CmdExecutorMock).AssertExpectations(t)
 	})
+
+	t.Run("add with -b option", func(t *testing.T) {
+		addCmd := cmd.NewAddCmd()
+		app := &core.App{
+			Exec:   utils.NewCmdExecutorMock(),
+			Git:    git.NewGitMock(),
+			Config: core.NewConfigMock(),
+		}
+
+		app.Git.(*git.GitMock).On("GetMainWorktree", app.Exec).Return(utils.Worktree{Branch: "refs/heads/main", Path: "/home/user/main"}, nil)
+		app.Git.(*git.GitMock).On("AddWorktree", "/home/user/worktrees/test_worktree", "FEAT-1", app.Exec, true).Return(nil)
+
+		app.Config.(*core.ConfigMock).On("LoadConfig").Return(nil)
+		app.Config.(*core.ConfigMock).On("GetInitCommands").Return([]string{"echo '1st command'", "echo '2nd command'"})
+
+		app.Exec.(*utils.CmdExecutorMock).On("StdOutPipe", "echo '1st command'").Return(nil)
+		app.Exec.(*utils.CmdExecutorMock).On("StdOutPipe", "echo '2nd command'").Return(nil)
+
+		ctx := context.WithValue(context.Background(), core.AppKey{}, app)
+		addCmd.SetContext(ctx)
+
+		addCmd.SetArgs([]string{"-b", "test_worktree", "FEAT-1"})
+		addCmd.Execute()
+
+		app.Git.(*git.GitMock).AssertExpectations(t)
+		app.Config.(*core.ConfigMock).AssertExpectations(t)
+		app.Exec.(*utils.CmdExecutorMock).AssertExpectations(t)
+	})
 }
